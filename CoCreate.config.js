237b0978e754341174cfcdc46bf78f25b11af068
Frank Pagan <frank@cocreate.app>
2022-11-22 12:36:23 +0000

    fix: apply src: {{source}} to CoCreate.config

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    config: {
+    "config": {
         "apiKey": "2061acef-0451-4545-f754-60cf8160",
         "organization_id": "5ff747727005da1c272740ab",
         "host": "general.cocreate.app"
@@ -8,11 +8,11 @@ module.exports = {
         {
             "entry": "./docs/index.html",
             "collection": "files",
-            "document_id": "6204253480b409001727b73e",
-            "key": "src",
             "document": {
+                "_id": "6204253480b409001727b73e",
                 "name": "index.html",
                 "path": "/docs/crud-server/index.html",
+                "src": "{{source}}",
                 "domains": [
                     "*",
                     "general.cocreate.app"
