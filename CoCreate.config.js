32633f701448af59dcb6da2339a9a43d6bda9612
frankpagan <frank@cocreate.app>
2023-09-17 19:12:09 -0600

    fix:  Add path and pathname

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,17 +8,17 @@ module.exports = {
             "object": {
                 "_id": "6204253480b409001727b73e",
                 "name": "index.html",
-                "path": "/docs/crud-server/index.html",
+                "path": "/docs/crud-server",
+                "pathname": "/docs/crud-server/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
                     "*",
                     "general.cocreate.app"
                 ],
-                "directory": "/docs/crud-server",
+                "directory": "crud-server",
                 "content-type": "text/html",
-                "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
+                "public": "true"
             }
         }
     ]
-}
+};
